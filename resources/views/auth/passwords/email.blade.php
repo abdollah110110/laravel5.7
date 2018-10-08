7c25ab62b7441c029481a319ae3cf04a473ef3f3
abdollah110110 <abdollah_sam@yahoo.com>
2018-10-08 14:06:19 +0330

    Send token for reset password

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,34 @@
-@extends('layouts.app')
+@extends('layouts.master')
+
+@section('title')
+- دریافت ایمیل
+@endsection
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+    <div class="mb-0 mt-4">
+        <h2>دریافت ایمیل</h2>
+    </div>
+    <hr />
+    
+    @include('includes.session-flash')
+    
+    <form action="{{ route('password.email') }}" method="POST" class="form-horizontal bg-light py-5 px-3">
+        {{ csrf_field() }}
+        <div class="form-group row {{ $errors->has('email') ? 'has-error' : '' }}">
+            <label for="email" class="col-lg-2 col-form-label text-lg-left">ایمیل:</label>
+            <div class="col-lg-10">
+                <input class="form-control" type="email" id="email" name="email" value="{{ old('email') != null ? old('email') : '' }}" placeholder="ایمیل را وارد کنید...">
+            </div>
+            <div class="col-lg-12 text-center mt-sm-1">
+                @if($errors->has('email'))
+                <small class="text-danger">{{ $errors->first('email') }}</small>
+                @endif
             </div>
         </div>
-    </div>
+        <div class="d-flex justify-content-center mt-4">
+            <button type="submit" class="btn btn-primary">ارسال لینک تغییر رمز عبور</button>
+        </div>
+    </form>
 </div>
 @endsection
