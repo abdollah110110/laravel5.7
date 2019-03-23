5c2928e197b4fff4ffdfbdccbe19dcfabe1f579b
abdollah110110 <abdollah_sam@yahoo.com>
2019-03-24 00:55:49 +0430

    Add navbar and dropdown and search to master layout

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -18,39 +18,56 @@
     </head>
     <body>
 
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown link
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-    </ul>
-  </div>
-</nav>
+		<nav class="navbar navbar-expand-md navbar-dark bg-success">
+			<a class="navbar-brand" href="#">شاین</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item active">
+						<a class="nav-link" href="#">صفحه اصلی</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">گیاهان دارویی</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">خشکبار</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">صنایع دستی</a>
+					</li>
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							موضوعات
+						</a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="#">گیاهان دارویی</a>
+							<a class="dropdown-item" href="#">خشکبار</a>
+							<a class="dropdown-item" href="#">صنایع دستی</a>
+						</div>
+					</li>
+<!--					<li class="nav-item">
+						<a class="nav-link disabled" href="#">Disabled</a>
+					</li>-->
+				</ul>
+			</div>
+		</nav>
+		
+		<div class="container">
+			<div class="row">
+				<form class="form-inline">
+					<input class="form-control" type="search" placeholder="جستجو کنید..." aria-label="جستجو">
+					<button class="btn btn-warning rounded-left" type="submit">جستجو</button>
+				</form>
+			</div>
+		</div>
 
         @yield('content')
 
         <script src="/js/jquery.js"></script>
         <script src="/js/bootstrap.min.js"></script>
+        <script src="/js/scripts.js"></script>
     </body>
 </html>
