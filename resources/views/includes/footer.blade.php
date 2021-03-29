2673ddb608a8018f3adc4a955cedc4791f26eb31
abdollah110110 <abdollah_sam@yahoo.com>
2021-03-29 17:35:15 +0430

    Add auth

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="footer">
+	<div class="container">
+		<span class="">کلیه حقوق این سایت متعلق به <a>بوت استرپ تکمیل</a> می باشد.</span>
+	</div>
+</footer>
---
+++ resources/views/includes/jumbotron.blade.php
@@ -0,0 +1,10 @@
+<!-- Begin page content -->
+<div class="container">
+	<div class="jumbotron w-100 text-center">
+		<div class="jumbo-text">
+			<h2 class="display-6"><i class="fas fa-user"></i> سلام کاربر گرامی،</h2>
+			<h1 class="display-5 mt-5">به سایت {{ config('app.name', 'Laravel') }} خوش آمدید</h1>
+			<p class="lead mt-5">می خواهیم با مواردی که یاد گرفتیم یه پروژه اجرا کنیم.</p>
+		</div>
+	</div>
+</div>
