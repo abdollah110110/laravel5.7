1a6135dbf6f5c1abb3963b7863f89b7db2210d82
abdollah110110 <abdollah_sam@yahoo.com>
2021-03-31 17:05:03 +0430

    Change migration users table and add Faker package and add user seeder.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,29 @@
 <?php
 
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
-    }
+class DatabaseSeeder extends Seeder {
+
+	/**
+	 * Seed the application's database.
+	 *
+	 * @return void
+	 */
+	public function run() {
+		$faker = Factory::create();
+
+		for ( $i = 0; $i < 5; $i ++ ) {
+			DB::table( 'users' )->insert( [
+				'name' => $faker->name,
+				'email' => $faker->email,
+				'password' => bcrypt( '123456' ),
+			] );
+		}
+
+//		$this->call(  UserFactory::class  );
+	}
+
 }
